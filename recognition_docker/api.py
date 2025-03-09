90235809fa5b9bfa1650e2e0b565387a84fa5f70
Slhboi <95510282+Slhboi@users.noreply.github.com>
2025-03-10 01:35:05 +0800

    Modifying of recognize() to allow for recognition without detect(). This was also tested to ensure it would give the same output as the original recognize() function.

--- recognition_docker/api.py
+++ recognition_docker/api.py
@@ -3,7 +3,7 @@ import cv2
 import mediapipe as mp
 import face_recognition
 import numpy as np
-from face_utils import  recognize_face  
+from face_utils import recognize_face  
 from face_detection import detect_faces
 
 app = FastAPI()
@@ -12,20 +12,21 @@ app = FastAPI()
 reference_image = face_recognition.load_image_file("moh.png")
 reference_encoding = face_recognition.face_encodings(reference_image)[0]
 
-
 @app.post("/recognize")
 async def recognize(file: UploadFile = File(...)):
     """Endpoint to recognize if the detected face matches the reference image."""
     contents = await file.read()
     nparr = np.frombuffer(contents, np.uint8)
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    faces = detect_faces(frame) ### i think this line here will be fine since it can still run detect_faces() from face_detection.py
+    """Removal of this section and input of direct frame works"""
+    # faces = detect_faces(frame) 
 
-    if not faces:
-        return {"message": "No faces detected"}
+    # if not faces:
+    #     return {"message": "No faces detected"}
 
-    x, y, width, height, face = faces[0]
-    is_moh = bool(recognize_face(face, reference_encoding))  # Convert numpy.bool_ to Python bool
+    # x, y, width, height, face = faces[0]
+    is_moh = bool(recognize_face(frame, reference_encoding))  # Convert numpy.bool_ to Python bool
 
     return {"match": is_moh}
+
 
