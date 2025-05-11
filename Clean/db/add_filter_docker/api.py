a0d34718779220acacb1ccd356476b311a3a1259
Mohtady-Ehab-Barakat <barakat.mohtady@gmail.com>
2025-05-11 17:36:42 +0800

    logger example at add_filter_docker

--- Clean/db/add_filter_docker/api.py
+++ Clean/db/add_filter_docker/api.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI, UploadFile, File, Form
 import shutil
 import os
 from add_filters import apply_filters
+import logging, sys
 #uvicorn api:app --host 0.0.0.0 --port 8083
 #docker build -t face-rec-api .
 #docker run -p 8083:8083 face-rec-api
 
 
 app = FastAPI()
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root_logger.addHandler(handler)
+
 
 UPLOAD_DIR = "../uploads"
 FILTERED_DIR = "../images_with_filters"
@@ -24,6 +35,8 @@ def save_uploaded_file(uploaded_file: UploadFile):
 @app.post("/add-filters")
 async def add_filters_endpoint(image: UploadFile = File(...), person_name: str = Form(...)):
     """Applies filters and saves images with person's name."""
+    logging.debug(f"[add-filters] Received image: {image.filename} for person: {person_name}")
     image_path = save_uploaded_file(image)
     apply_filters(image_path, FILTERED_DIR, person_name)
+    logging.debug(f"[add-filters] Filters applied and saved to {FILTERED_DIR} for {person_name}")
     return {"message": f"Filters applied for {person_name}", "output_directory": FILTERED_DIR}
