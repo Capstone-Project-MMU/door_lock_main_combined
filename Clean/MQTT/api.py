83057b8eb88a83208765c443f568e71752ba60dd
Mohtady-Ehab-Barakat <barakat.mohtady@gmail.com>
2025-06-08 02:45:20 +0800

    none

---
+++ Clean/MQTT/api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import paho.mqtt.publish as publish
+
+app = FastAPI()
+
+MQTT_BROKER = "192.168.100.212"
+MQTT_TOPIC = "home/text_message"
+MQTT_USER = "myuser"
+MQTT_PASS = "mypassword"
+
+class Message(BaseModel):
+    word: str
+
+@app.post("/send-word")
+def send_word(msg: Message):
+    publish.single(
+        topic=MQTT_TOPIC,
+        payload=msg.word,
+        hostname=MQTT_BROKER,
+        auth={'username': MQTT_USER, 'password': MQTT_PASS}
+    )
+    return {"status": "sent", "word": msg.word}
